5da109c9bc1b364fb36dccb3d22e7d15a6bd5957
Theodore R. Smith <theodore@phpexperts.pro>
2012-01-27 15:14:40 -0600

    Added licensing boilerplate.

--- ThriveCalendar.php
+++ ThriveCalendar.php
@@ -1,22 +1,32 @@
 <?php
-/**
-* Thrive Calendar
-*   Copyright © 2011 
-*       Theodore R. Smith <theodore@phpexperts.pro>
-*   -and-
-*       Monica Chase <chase.mono@gmail.com>
-*
-* The following code is licensed under a modified BSD License.
-* All of the terms and conditions of the BSD License apply with one
-* exception:
-*
-* 1. Every one who has not been a registered student of the "PHPExperts
-*    From Beginner To Pro" course (http://www.phpexperts.pro/) is forbidden
-*    from modifing this code or using in an another project, either as a
-*    deritvative work or stand-alone.
-*
-* BSD License: http://www.opensource.org/licenses/bsd-license.php
-**/
+// Thrive Calendar
+// This file is a part of the Thrive Framework, a PHPExperts.pro Project.
+//
+// Copyright (c) 2012 Theodore R.Smith (theodore@phpexperts.pro)
+// DSA-1024 Fingerprint: 10A0 6372 9092 85A2 BB7F  907B CB8B 654B E33B F1ED
+// Provided by the PHP University (www.phpu.cc) and PHPExperts.pro (www.phpexperts.pro)
+//
+// This file is dually licensed under the terms of the following licenses:
+// * Primary License: OSSAL v1.0 - Open Source Software Alliance License
+//   * Key points:
+//       5.Redistributions of source code in any non-textual form (i.e.
+//          binary or object form, etc.) must not be linked to software that is
+//          released with a license that requires disclosure of source code
+//          (ex: the GPL).
+//       6.Redistributions of source code must be licensed under more than one
+//          license and must not have the terms of the OSSAL removed.
+//   * See LICENSE.ossal for complete details.
+//
+// * Secondary License: Creative Commons Attribution License v3.0
+//   * Key Points:
+//       * You are free:
+//           * to copy, distribute, display, and perform the work
+//           * to make non-commercial or commercial use of the work in its original form
+//       * Under the following conditions:
+//           * Attribution. You must give the original author credit. You must retain all
+//             Copyright notices and you must include the sentence, "Based upon work from
+//             PHPExperts.pro (www.phpexperts.pro).", wherever you list contributors.
+//   * See LICENSE.cc_by for complete details.
 
 abstract class ThriveCalendarLogic
 {
